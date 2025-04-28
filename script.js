b548de7f9070ab64a63ef9a0654e3132ab39bb34
Bruno Seiji <88354853+bruno-seiji@users.noreply.github.com>
2025-04-28 14:32:44 -0300

    XP ok

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
 var lista_places =  document.querySelectorAll(".lista_option")
+var lista_infs =  document.querySelectorAll(".lista_option_infs")
 
 function select_one(){
     lista_places[0].classList.add("green")
     lista_places[1].classList.remove("green")
     lista_places[2].classList.remove("green")
-    evt.preventDefault();
+    lista_infs[0].style.display = "flex"
+    lista_infs[1].style.display = "none"
+    lista_infs[2].style.display = "none"
+
 }
 
-function select_two{}
+function select_two(){
+    lista_places[1].classList.add("green")
+    lista_places[0].classList.remove("green")
+    lista_places[2].classList.remove("green")
+    lista_infs[1].style.display = "flex"
+    lista_infs[0].style.display = "none"
+    lista_infs[2].style.display = "none"
 
-function select_three{}
+}
+
+function select_three(){
+    lista_places[2].classList.add("green")
+    lista_places[0].classList.remove("green")
+    lista_places[1].classList.remove("green")
+    lista_infs[2].style.display = "flex"
+    lista_infs[0].style.display = "none"
+    lista_infs[1].style.display = "none"
+
+}
+
+window.addEventListener('load', () => {
+    select_one();
+  });
+
