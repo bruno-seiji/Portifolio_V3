c862715283e1ba31b7ab0cdee9ccbf40e627264a
Bruno Seiji <88354853+bruno-seiji@users.noreply.github.com>
2025-04-28 13:49:17 -0300

    .

---
+++ script.js
@@ -0,0 +1,12 @@
+var lista_places =  document.querySelectorAll(".lista_option")
+
+function select_one(){
+    lista_places[0].classList.add("green")
+    lista_places[1].classList.remove("green")
+    lista_places[2].classList.remove("green")
+    evt.preventDefault();
+}
+
+function select_two{}
+
+function select_three{}
